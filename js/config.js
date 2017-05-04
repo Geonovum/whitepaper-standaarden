d0e1f1e829fd10a9bbc743afd9b8588118a066f3
Linda van den Brink <l.vandenbrink@geonovum.nl>
2017-05-04 15:55:42 +0200

    add js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+   var respecConfig = {
+      specStatus: "GN-BASIS",
+      specType: "IM",
+      editors: [{
+        name: "Friso Penninga",
+        company: "Geonovum",
+        url: "http://www.geonovum.nl/",
+        },{
+        name: "Linda van den Brink",
+        company: "Geonovum",
+        url: "http://www.geonovum.nl/",
+      }],
+      shortName: "whitepaper-standaarden",
+      subtitle: "Conceptversie in ontwikkeling",
+      github: "https://github.com/Geonovum/whitepaper-standaarden",
+      issueBase: "https://github.com/Geonovum/whitepaper-standaarden/issues/"
+    };
