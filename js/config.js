ec12f978db8bfb41aa487fcffe75ecb014758e9e
Linda van den Brink <l.vandenbrink@geonovum.nl>
2017-08-14 16:31:11 +0200

    added references, related to #17

--- js/config.js
+++ js/config.js
@@ -19,4 +19,62 @@ var respecConfig = {
   subtitle: "Werkversie",
   github: "https://github.com/Geonovum/whitepaper-standaarden",
   issueBase: "https://github.com/Geonovum/whitepaper-standaarden/issues/",
+  localBiblio: {
+    NEN3610: {
+      href: "http://www.nen.nl/web/Normshop/Norm/NEN-36102011-nl.htm",
+      title:
+        "Basismodel Geo-informatie - Termen, definities, relaties en algemene regels voor de uitwisseling van informatie over aan de aarde gerelateerde ruimtelijke objecten",
+      authors: [""],
+      date: "Maart 2011",
+      publisher: "Nederlands Normalisatie-instituut",
+    },
+    "WMS-NL": {
+      href:
+        "https://www.geonovum.nl/wegwijzer/standaarden/nederlands-profiel-web-map-service-op-iso-19128-versie-11",
+      title:
+        "Nederlands profiel op ISO 19128 Geographic information — Web Map Server Interface",
+      authors: ["Geonovum"],
+      date: "24 april 2015",
+    },
+    "WFS-NL": {
+      href:
+        "https://www.geonovum.nl/wegwijzer/standaarden/nederlands-wfs-profiel-11-op-iso-19142-voor-web-feature-services-20",
+      title:
+        "Nederlands profiel op ISO 19142 Geographic information — Web Feature Service",
+      authors: ["Geonovum"],
+      date: "24 april 2015",
+    },
+    WFS: {
+      href: "http://www.opengeospatial.org/standards/wfs",
+      publisher: "OGC",
+      authors: ["Panagiotis (Peter) A. Vretanos"],
+      date: "10 July 2014",
+      title: "Web Feature Service 2.0 Interface Standard",
+      status: "OGC Interface Standard",
+    },
+    WMS: {
+      href: "http://www.opengeospatial.org/standards/wms",
+      publisher: "OGC",
+      authors: ["Jeff de la Beaujardiere"],
+      date: "15 March 2006",
+      title: "Web Map Service Implementation Specification",
+      status: "OpenGIS Implementation Standard",
+    },
+    GeoPackage: {
+      href: "http://www.opengeospatial.org/standards/geopackage",
+      publisher: "OGC",
+      authors: ["Jeff Yutzler"],
+      date: "4 August 2015",
+      title: "OGC GeoPackage Encoding Standard",
+      status: "OpenGIS Implementation Standard",
+    },
+    topic20: {
+      href: "http://www.opengeospatial.org/standards/om",
+      publisher: "OGC",
+      authors: ["Simon Cox"],
+      date: "17 September 2013",
+      title: "Geographic information — Observations and measurements",
+      status: "OGC Abstract Specification",
+    },
+  },
 };
